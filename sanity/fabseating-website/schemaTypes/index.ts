9d0b9f5b641221a21e7f2e88b3c5deccc529a1a2
Aryabhatta <aryabhattajr@gmail.com>
2026-03-07 15:59:13 +0530

    Refactor schema imports and update home page title validation
    - Removed unused schema imports from index files to streamline schema management.
    - Updated the home page schema to remove the required validation on the title field.

--- sanity/fabseating-website/schemaTypes/index.ts
+++ sanity/fabseating-website/schemaTypes/index.ts
@@ -1,12 +1,6 @@
-import productCategory from '../../schemas/productCategory'
-import product from '../../schemas/product'
 import project from '../../schemas/project'
 import blogPost from '../../schemas/blogPost'
-import author from '../../schemas/author'
-import testimonial from '../../schemas/testimonial'
-import faq from '../../schemas/faq'
 import siteSettings from '../../schemas/siteSettings'
-import landingPage from '../../schemas/landingPage'
 import solutionPage from '../../schemas/solutionPage'
 import catalog from '../../schemas/catalog'
 import caseStudy from '../../schemas/caseStudy'
@@ -15,16 +9,10 @@ import contactPage from '../../schemas/contactPage'
 import teamMember from '../../schemas/teamMember'
 
 export const schemaTypes = [
-  productCategory,
-  product,
   project,
   caseStudy,
   blogPost,
-  author,
-  testimonial,
-  faq,
   siteSettings,
-  landingPage,
   solutionPage,
   catalog,
   homePage,
--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,12 +1,6 @@
-import productCategory from './productCategory'
-import product from './product'
 import project from './project'
 import blogPost from './blogPost'
-import author from './author'
-import testimonial from './testimonial'
-import faq from './faq'
 import siteSettings from './siteSettings'
-import landingPage from './landingPage'
 import solutionPage from './solutionPage'
 import catalog from './catalog'
 import caseStudy from './caseStudy'
@@ -15,16 +9,10 @@ import contactPage from './contactPage'
 import teamMember from './teamMember'
 
 export const schemaTypes = [
-  productCategory,
-  product,
   project,
   caseStudy,
   blogPost,
-  author,
-  testimonial,
-  faq,
   siteSettings,
-  landingPage,
   solutionPage,
   catalog,
   homePage,
